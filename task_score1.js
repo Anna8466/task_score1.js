cbbb9495378781fd76217dd5c8d327dc90c50fee
Anna Tr <anna.trandafil14@gmail.com>
2024-09-02 21:02:20 +0200

    h-w-2-1-3.md

---
+++ task_score1.js
@@ -0,0 +1,9 @@
+
+let studentName = "Oksana"
+let studentSurname = "Melnikova"
+
+let introModuleScore = 50, gitModuleScore = 70, jsModuleScore = 3;
+
+let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
+
+console.log(`The average score for ${studentName} ${studentSurname} is ${averageScore}`)
